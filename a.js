61c69602db804c631f80994ad94e8e2fd1f04628
luminietos <helenapaloheimo@gmail.com>
2022-05-03 20:08:33 +0300

    changes

--- a.js
+++ a.js
@@ -2,26 +2,43 @@
 let station_1 = document.getElementById("station_1").value.trim();
 
 //THE URL
-//note: added CORS headers to the proxied request 
-//in order to bypass error: 
-//"No 'Access-Control-Allow-Origin"
 let api_url = `https://rata.digitraffic.fi/api/v1/live-trains/station/${station_1}`
 const specs = `?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false`;
 const url = api_url + specs;
 
 //FUNCTIONS
-function getData() {
+async function getData() {
+    const station_1 = document.getElementById("station_1").value.trim();
     //if userinput is null, app gives an error message
-    return fetch(url, { method: "GET" })
-        .then((response) => response.json())
-        .then((responseData) => {
-            console.log(responseData);
-            return responseData;
-        })
-        .catch(error => console.warn(error));
-}
+    if (!station_1) return alert("Enter a station, please.");
 
-getData().then(response => response.value = JSON.stringify(data, null, 4));
+    //external API's url with the station being 
+    //a variable that changes via userinput 
+    let api_url = `https://rata.digitraffic.fi/api/v1/live-trains/station/${station_1}`;
+    const specs = `?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false`;
+    const url = api_url + specs;
+
+    try {
+        //fetching the text-area's id from HTML, assigning value
+        document.getElementById("traindata").value = "Loading...";
+        //calling fetch, passing in the url
+        const response = await fetch(url);
+
+        //if the response is not acceptable...
+        if (!response.ok) throw new Error("Error");
+        const data = await response.json();
+
+        //displays the response to the userinput
+        document.getElementById("traindata").value = JSON.stringify(data, null, 4);
+    } catch (error) {
+        //error message
+        alert(error);
+    }
+
+    if (station_1) {
+        //if station_1's input is fetched correctly...
+    }
+}
 
 
 //"RAW DATA" (CONTAINER) FUNCTION
