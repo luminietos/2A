84d5234811eafd6fd61b8771d93be65a73c6548c
luminietos <helenapaloheimo@gmail.com>
2022-05-03 19:23:04 +0300

    updated everything.
    I keep forgetting committing to main is a thing<3

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# 2A
- 
+ # Project 02a 
+ Project started in May 2022.
+[Project's Github link]() & [Project's Netlify link]()
+
+## Description
+This is a Finnish train station information app that that utilizes Live JSON API from Digitraffic. 
+It is my second JS project and my first project using an API, made for my University's JavaScript course. On top of JS, it has CSS and HTML for the UI (layout & style). 
+
+## User Instructions - *"How do I actually use this thing?"*
+There are two green containers with two different text fields.
+In the first text field, the user types in a Finnish train station name's short-code (i.e. 'HKI' for 'Helsinki') text field. Once they've done this, they may click on the magnifying glass icon aka the search button, which will retrieve the search result for the user. 
+Data regarding the station the user input will appear below.
+
+Below this is the aforementioned second green container. It functions like the one above, only this one contains the raw, live data regarding the station the user types into the text field. 
+   The textarea expands on click, and will shrink back if the user clicks outside the text area (*unless* the text area already contains data from the user's input).
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Credit, sources
+The data is sourced from [Digitraffic's API](https://www.digitraffic.fi/rautatieliikenne/#junien-tiedot-trains).
+
+## Project Status
+This project is under development. 
--- a.js
+++ a.js
@@ -1,18 +1,57 @@
-//alternatively: document.getElementById('submit_btn).addEventListener ('click', getTrainData);
-const submit_btn = document.getElementById('submit_btn');
-submit_btn.addEventListener('click', getTrainData)
+//establishing userinput into a separate variable
+let station_1 = document.getElementById("station_1").value.trim();
 
-//the API url we're retrieving data from
-var api ="https://rata.digitraffic.fi/api/v1/live-trains/station/";
-//gets the desired userinput station (from the html)
-input = select.getElementById(station);
-//arrived/arriving & departed/departing trains' schedules
-var schedules = "?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false";
+//THE URL
+//note: added CORS headers to the proxied request 
+//in order to bypass error: 
+//"No 'Access-Control-Allow-Origin"
+let api_url = `https://rata.digitraffic.fi/api/v1/live-trains/station/${station_1}`
+const specs = `?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false`;
+const url = api_url + specs;
 
-//function
-function getTrainData() {
-    //the url is the api path + the userinput parameters (station)
-    var url = api + input.value() + schedules;
+//FUNCTIONS
+function getData() {
+    //if userinput is null, app gives an error message
+    return fetch(url, { method: "GET" })
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log(responseData);
+            return responseData;
+        })
+        .catch(error => console.warn(error));
 }
-   
-getTrainData();
+
+getData().then(response => response.value = JSON.stringify(data, null, 4));
+
+
+//"RAW DATA" (CONTAINER) FUNCTION
+async function getTrainData() {
+    const station = document.getElementById("station").value.trim();
+    //if userinput is null, app gives an error message
+    if (!station) return alert("Enter a station, please.");
+
+    //external API's url with the station being 
+    //a variable that changes via userinput 
+    let api_url = `https://rata.digitraffic.fi/api/v1/live-trains/station/${station}`;
+    const specs = `?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false`;
+    const url = api_url + specs;
+    //const second_url = `https://rata.digitraffic.fi/api/v1/metadata/stations`;
+    //||
+
+    try {
+        //fetching the text-area's id from HTML, assigning value
+        document.getElementById("rawdata").value = "Loading...";
+        //calling fetch, passing in the url
+        const response = await fetch(url);
+
+        //if the response is not acceptable...
+        if (!response.ok) throw new Error("Error");
+        const data = await response.json();
+
+        //displays the response to the userinput
+        document.getElementById("rawdata").value = JSON.stringify(data, null, 4);
+    } catch (error) {
+        //error message
+        alert(error);
+    }
+}
